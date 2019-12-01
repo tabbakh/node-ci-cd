1f7526916b004a466d18d2c41679bd0c17f71491
Abderrahmen <atabbakh@lefigaro.fr>
2019-12-01 18:33:53 +0100

    test36

--- main.js
+++ main.js
@@ -4,7 +4,7 @@ var http = require('http');
 // configure our HTTP server
 var server = http.createServer(function (request, response) {
   response.writeHead(200, {"Content-Type": "text/plain"});
-  response.end("Hello Sara\n");
+  response.end("Hello Adem\n");
 });
 
 // listen on localhost:8000
