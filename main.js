956d5aa2d43bf71e42d31d7d936bb4e4350f27d6
Abderrahmen <atabbakh@lefigaro.fr>
2019-12-01 18:45:26 +0100

    change msg to bonjour tout le monde

--- main.js
+++ main.js
@@ -4,7 +4,7 @@ var http = require('http');
 // configure our HTTP server
 var server = http.createServer(function (request, response) {
   response.writeHead(200, {"Content-Type": "text/plain"});
-  response.end("Hello Adem\n");
+  response.end("Bonjour tout le monde\n");
 });
 
 // listen on localhost:8000
