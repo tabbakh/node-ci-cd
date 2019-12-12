5bb3dfb3aba1db41273fcfa14a1b165af71f8d16
Abderrahmen <atabbakh@lefigaro.fr>
2019-12-12 21:49:51 +0100

    chg msg to hello world

--- main.js
+++ main.js
@@ -4,7 +4,7 @@ var http = require('http');
 // configure our HTTP server
 var server = http.createServer(function (request, response) {
   response.writeHead(200, {"Content-Type": "text/plain"});
-  response.end("Hello tout le monde\n");
+  response.end("Hello world\n");
 });
 
 // listen on localhost:8000
